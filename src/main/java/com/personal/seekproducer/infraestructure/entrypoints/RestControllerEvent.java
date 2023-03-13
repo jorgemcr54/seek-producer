de09b7fc32e572a444c24d2a8b447073034daae2
Jorge Espinosa <joeespin@bancolombia.com.co>
2023-03-13 17:10:41 -0500

    creacion del cloudevent para la clase place to hide

--- src/main/java/com/personal/seekproducer/infraestructure/entrypoints/RestControllerEvent.java
+++ src/main/java/com/personal/seekproducer/infraestructure/entrypoints/RestControllerEvent.java
@@ -1,14 +1,24 @@
 package com.personal.seekproducer.infraestructure.entrypoints;
 
 import com.personal.seekproducer.domain.model.PlaceToHide;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.spring.http.CloudEventHttpUtils;
+import io.cloudevents.core.builder.CloudEventBuilder;
 import org.reactivecommons.api.domain.DomainEvent;
 import org.reactivecommons.api.domain.DomainEventBus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.security.Timestamp;
+import java.time.OffsetDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 @RestController
@@ -18,11 +28,20 @@ public class RestControllerEvent {
     DomainEventBus eventBus;
 
     @GetMapping("/search/{floor}/{room}")
-    public Mono<String> sendEvent(@PathVariable int floor,@PathVariable String room){
+    public Mono<String> sendEvent(@PathVariable int floor,@PathVariable String room, @RequestHeader HttpHeaders headers){
         PlaceToHide placeToHide = new PlaceToHide();
         placeToHide.setFloor(floor);
         placeToHide.setRoom(room);
-        DomainEvent<PlaceToHide> event = new DomainEvent<PlaceToHide>("place.searched", UUID.randomUUID().toString(),placeToHide);
+        CloudEvent attributes = CloudEventBuilder.v1() //
+                .withId(UUID.randomUUID().toString()) //
+                .withSource(URI.create("https://spring.io/foos"))//
+                .withType("event") //
+                .withDataContentType("application/json")
+                .withTime(OffsetDateTime.now())
+                .withData("text/plain", String.valueOf(placeToHide).getBytes(StandardCharsets.UTF_8))
+                .build();
+
+        DomainEvent<CloudEvent> event = new DomainEvent<CloudEvent>("place.searched", UUID.randomUUID().toString(), attributes);
         return Mono.from(eventBus.emit(event)).thenReturn("Place searched");
     }
 
